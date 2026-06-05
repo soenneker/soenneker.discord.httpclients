70defa93ae2aa3abc054b5ffae782b24a7b73b8c
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:08 -0500

    xml commenting

--- src/Soenneker.Discord.HttpClients/DiscordOpenApiHttpClient.cs
+++ src/Soenneker.Discord.HttpClients/DiscordOpenApiHttpClient.cs
@@ -45,11 +45,18 @@ public sealed class DiscordOpenApiHttpClient : IDiscordOpenApiHttpClient
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(DiscordOpenApiHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(DiscordOpenApiHttpClient));
